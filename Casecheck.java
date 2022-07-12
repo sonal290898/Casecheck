0c4bf805fc68d0005ae07b83e3e7082ad39881f7
Sonal Kumar <75514302+sonal290898@users.noreply.github.com>
2022-07-12 13:34:51 +0530

    Update and rename README.md to  Casecheck.java

---
+++ Casecheck.java
@@ -0,0 +1,15 @@
+# Casecheck
+Check case are in Lower or upper class
+  import java.util.Scanner;
+ public class Checkclass{
+     public static void main(String args []){
+     Scanner sc=new Scanner(System.in);
+     char ch=sc.next().trim().charAt(0);
+    if(ch>='a' && cha<='z'){
+         System.out.println("Lowercase");
+     }
+     else{
+         System.out.println("Uppercase");
+         }
+ }
+ }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Casecheck
-Check case are in Lower or upper class
